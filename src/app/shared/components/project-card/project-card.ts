dd2cab965945b4503bcc91e2666158a003865674
Aloïs Drucké <alois.drucke.ext@allianz-trade.com>
2026-08-20 16:01:33 +0200

    feat: add Projects page with computed filter and ProjectCard component

---
+++ src/app/shared/components/project-card/project-card.ts
@@ -0,0 +1,14 @@
+import { Component, inject, input } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { Project } from '../../../core/models/project.model'
+import { I18nService } from '../../../core/services/i18n.service'
+
+@Component({
+  selector: 'app-project-card',
+  imports: [RouterLink],
+  templateUrl: './project-card.html',
+})
+export class ProjectCardComponent {
+  project = input.required<Project>()
+  protected i18n = inject(I18nService)
+}
