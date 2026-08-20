41ab752177aee0670377ed8367ca2d5fa72f1cac
Aloïs Drucké <alois.drucke.ext@allianz-trade.com>
2026-08-20 15:37:33 +0200

    feat: add Experience page with TimelineItem component and @for

--- src/app/features/experience/experience.ts
+++ src/app/features/experience/experience.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { toSignal } from '@angular/core/rxjs-interop'
+import { PortfolioService } from '../../core/services/portfolio.service'
+import { I18nService } from '../../core/services/i18n.service'
+import { TimelineItemComponent } from '../../shared/components/timeline-item/timeline-item'
 
 @Component({
   selector: 'app-experience',
-  template: '<p>Experience – coming soon</p>',
+  imports: [TimelineItemComponent],
+  templateUrl: './experience.html',
 })
-export class ExperienceComponent {}
+export class ExperienceComponent {
+  protected i18n = inject(I18nService)
+  private portfolioService = inject(PortfolioService)
+
+  protected experiences = toSignal(this.portfolioService.getExperiences(), { initialValue: [] })
+}
---
+++ src/app/shared/components/timeline-item/timeline-item.ts
@@ -0,0 +1,12 @@
+import { Component, inject, input } from '@angular/core'
+import { Experience } from '../../../core/models/experience.model'
+import { I18nService } from '../../../core/services/i18n.service'
+
+@Component({
+  selector: 'app-timeline-item',
+  templateUrl: './timeline-item.html',
+})
+export class TimelineItemComponent {
+  experience = input.required<Experience>()
+  protected i18n = inject(I18nService)
+}
