1b23a2bdd97aaecd037d9929e4ded97ca729b0ad
Aloïs Drucké <alois.drucke.ext@allianz-trade.com>
2026-08-20 15:23:21 +0200

    feat: add About page with toSignal HTTP pattern

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { toSignal } from '@angular/core/rxjs-interop'
+import { PortfolioService } from '../../core/services/portfolio.service'
+import { I18nService } from '../../core/services/i18n.service'
 
 @Component({
   selector: 'app-about',
-  template: '<p>About – coming soon</p>',
+  templateUrl: './about.html',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  protected i18n = inject(I18nService)
+  private portfolioService = inject(PortfolioService)
+
+  protected about = toSignal(this.portfolioService.getAbout())
+}
